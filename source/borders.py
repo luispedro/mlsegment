5054519d22e03fee9e85256e0481e4252e4ed9be
Luis Pedro Coelho <lpc@cmu.edu>
2010-11-29 14:58:41 -0500

    ENH: Some border features

--- source/borders.py
+++ source/borders.py
@@ -3,15 +3,31 @@
 # License: MIT
 
 from __future__ import division
+from _borders import borders
+
+def border_features(img, labeled, i, j, border):
+    inside = (labeled == i)|(labeled == j)
+    pixels = img[inside].ravel()
+    borderp = img[border].ravel()
+    intensity = (pixels.mean() - borderp.mean())/(1.+pixels.std())
+    edginess = sobel[border].mean()/(1.+sobel[inside])
+    pixels_i = img[labeled == i].ravel()
+    pixels_j = img[labeled == j].ravel()
+    separation = np.abs(pixels_i.mean() - pixels_j.mean())/(pixels_i.std() + pixels_j.std() + 40/pixels_i.size + 40/pixels_j.size)
+    return np.array([intensity, edginess,separation])
 
 def borders(img, solution, iborder_model):
     labeled, n_regions = solution
     nborders = 0.
+    filter = np.ones((3,3), dtype=labeled.dtype)
+    output = np.zeros(labeled.shape, bool)
+
     for i in xrange(n_regions):
         for j in xrange(i+1, n_regions):
-            if has_border(labeled, i, j):
-                border = border_for(solution, i, j)
-                val += bborder_model.apply(border_features(img, border))
+            output.fill(False)
+            border = borders(labeled, filter, output, i, j, 0)
+            if border is not None:
+                val += bborder_model.apply(border_features(img, labeled, i, j, border))
                 nborders += 1.
     return val/nborders
 
